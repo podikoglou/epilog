76252b89b54c780b5bec4b5c62c88049648fabcc
alex <alex.podikoglou@gmail.com>
2026-05-31 21:56:57 +0300

    term

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+#[derive(Clone, Debug, PartialEq, Eq, Hash)]
+pub enum Term {
+    Atom(String),
+    Var(String),
+    Compound(String, Vec<Term>),
+}
+
 fn main() {
     println!("Hello, world!");
 }
