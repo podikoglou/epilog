71c4930b3dd418cdb5fe045bc8bf39537976a419
alex <alex.podikoglou@gmail.com>
2026-05-31 21:58:34 +0300

    clause

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,22 @@ pub enum Term {
     Compound(String, Vec<Term>),
 }
 
+#[derive(Clone, Debug, PartialEq, Eq, Hash)]
+pub struct Clause {
+    head: Term,
+    body: Vec<Term>,
+}
+
+impl Clause {
+    pub fn fact(head: Term) -> Self {
+        Clause { head, body: vec![] }
+    }
+
+    pub fn rule(head: Term, body: Vec<Term>) -> Self {
+        Clause { head, body }
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
