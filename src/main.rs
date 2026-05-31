266a54a6df828af8cca8a158cb2da950dbe88524
alex <alex.podikoglou@gmail.com>
2026-05-31 22:01:22 +0300

    move to ast

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,4 @@
-#[derive(Clone, Debug, PartialEq, Eq, Hash)]
-pub enum Term {
-    Atom(String),
-    Var(String),
-    Compound(String, Vec<Term>),
-}
-
-#[derive(Clone, Debug, PartialEq, Eq, Hash)]
-pub struct Clause {
-    head: Term,
-    body: Vec<Term>,
-}
-
-impl Clause {
-    pub fn fact(head: Term) -> Self {
-        Clause { head, body: vec![] }
-    }
-
-    pub fn rule(head: Term, body: Vec<Term>) -> Self {
-        Clause { head, body }
-    }
-}
+pub mod ast;
 
 fn main() {
     println!("Hello, world!");
