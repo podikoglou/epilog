1f52580aaa3f759723ba50cb82bc4c600480b57c
alex <alex.podikoglou@gmail.com>
2026-06-01 12:17:53 +0300

    unify::{Substitutions,apply}

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 pub mod ast;
+pub mod unify;
 
 #[cfg(test)]
 mod tests;
---
+++ src/unify.rs
@@ -0,0 +1,19 @@
+use std::collections::HashMap;
+
+use crate::ast::Term;
+
+type Substitutions = HashMap<String, Term>;
+
+pub fn apply(substitutions: Substitutions, term: &Term) -> Term {
+    match term {
+        Term::Atom(_) => term.clone(),
+        Term::Var(var) => substitutions.get(var).unwrap_or(term).clone(),
+        Term::Compound(atom, terms) => Term::Compound(
+            atom.clone(),
+            terms
+                .iter()
+                .map(|term| apply(substitutions.clone(), term))
+                .collect::<Vec<_>>(),
+        ),
+    }
+}
