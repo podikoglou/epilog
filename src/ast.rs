a11d865d338bbe1bd722db6c5066765d0cf98cb0
alex <alex.podikoglou@gmail.com>
2026-06-01 11:44:41 +0300

    pretty printing

--- src/ast.rs
+++ src/ast.rs
@@ -1,8 +1,29 @@
+use std::fmt::Display;
+
 #[derive(Clone, Debug, PartialEq, Eq, Hash)]
 pub enum Term {
     Atom(String),
     Var(String),
     Compound(String, Vec<Term>),
+}
+
+impl Display for Term {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Term::Atom(atom) => write!(f, "{}", atom),
+            Term::Var(var) => write!(f, "{}", var),
+            Term::Compound(atom, terms) => {
+                write!(
+                    f,
+                    "{}({})",
+                    atom,
+                    terms
+                        .iter()
+                        .fold(String::default(), |acc, term| format!("{}, {}", acc, term))
+                )
+            }
+        }
+    }
 }
 
 #[derive(Clone, Debug, PartialEq, Eq, Hash)]
@@ -18,5 +39,22 @@ impl Clause {
 
     pub fn rule(head: Term, body: Vec<Term>) -> Self {
         Clause { head, body }
+    }
+}
+
+impl Display for Clause {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        if self.body.is_empty() {
+            write!(f, "{}.", self.head)
+        } else {
+            write!(
+                f,
+                "{} :- {}",
+                self.head,
+                self.body
+                    .iter()
+                    .fold(String::default(), |acc, term| format!("{}, {}", acc, term))
+            )
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,69 @@
 pub mod ast;
 
 use lalrpop_util::lalrpop_mod;
+
+use crate::ast::{Clause, Term};
 lalrpop_mod!(pub parser);
 
 fn main() {
-    println!("Hello, world!");
+    let clauses = vec![
+        Clause::fact(Term::Compound(
+            String::from("parent"),
+            vec![
+                Term::Atom(String::from("john")),
+                Term::Atom(String::from("mary")),
+            ],
+        )),
+        Clause::fact(Term::Compound(
+            String::from("parent"),
+            vec![
+                Term::Atom(String::from("john")),
+                Term::Atom(String::from("david")),
+            ],
+        )),
+        Clause::fact(Term::Compound(
+            String::from("parent"),
+            vec![
+                Term::Atom(String::from("mary")),
+                Term::Atom(String::from("alice")),
+            ],
+        )),
+        Clause::fact(Term::Compound(
+            String::from("parent"),
+            vec![
+                Term::Atom(String::from("david")),
+                Term::Atom(String::from("bob")),
+            ],
+        )),
+        Clause::rule(
+            Term::Compound(
+                String::from("ancestor"),
+                vec![Term::Var(String::from("X")), Term::Var(String::from("Y"))],
+            ),
+            vec![Term::Compound(
+                String::from("parent"),
+                vec![Term::Var(String::from("X")), Term::Var(String::from("Y"))],
+            )],
+        ),
+        Clause::rule(
+            Term::Compound(
+                String::from("ancestor"),
+                vec![Term::Var(String::from("X")), Term::Var(String::from("Y"))],
+            ),
+            vec![
+                Term::Compound(
+                    String::from("parent"),
+                    vec![Term::Var(String::from("X")), Term::Var(String::from("Z"))],
+                ),
+                Term::Compound(
+                    String::from("ancestor"),
+                    vec![Term::Var(String::from("Z")), Term::Var(String::from("Y"))],
+                ),
+            ],
+        ),
+    ];
+
+    for clause in clauses {
+        println!("{}", clause)
+    }
 }
