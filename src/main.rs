29fee8173dbb8fcbba33b08b78b781e37a2a691a
alex <alex.podikoglou@gmail.com>
2026-06-02 17:38:12 +0300

    load exmaple program in main

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,21 @@ pub mod unify;
 #[cfg(test)]
 mod tests;
 
+use std::fs;
+
 use lalrpop_util::lalrpop_mod;
+
+use crate::ast::Program;
 
 lalrpop_mod!(pub parser);
 
-fn main() {}
+fn main() {
+    let input = fs::read_to_string("examples/family.pl").expect("couldn't read examples/family.pl");
+    let par = parser::ProgramParser::new();
+
+    let program: Program = par.parse(&input).expect("couldn't parse");
+
+    for clause in program {
+        println!("{}", clause);
+    }
+}
